10afb44783e0ea48c9be76d0b268f82270dc2ff5
asmaa497 <sososalma33333@gmail.com>
2022-01-31 13:30:23 +0200

    addition

---
+++ ConsoleApp1/AdditionClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class AdditionClass
+    {
+        public int addnums(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
